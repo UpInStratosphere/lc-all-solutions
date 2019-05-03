66fc37b8b6fd0fd5e794958e996fb760d5584f51
UpInStratosphere <38412253+UpInStratosphere@users.noreply.github.com>
2019-05-03 16:49:16 -0400

    Update one-edit-distance.py

--- 161.one-edit-distance/one-edit-distance.py
+++ 161.one-edit-distance/one-edit-distance.py
@@ -12,3 +12,41 @@ class Solution(object):
       return s != t and (s[i + 1:] == t[i:] if len(s) != len(t) else s[i + 1:] == t[i + 1:])
     else:
       return self.isOneEditDistance(t, s)
+
+   
+  
+    #solution 2
+    m = len(s1) 
+    n = len(s2) 
+  
+    if abs(m - n) > 1: 
+        return false 
+  
+    count = 0    
+  
+    i = 0
+    j = 0
+    while i < m and j < n: 
+        if s1[i] != s2[j]: 
+            if count == 1: 
+                return false 
+            
+            if m > n: 
+                i+=1
+            elif m < n: 
+                j+=1
+                
+            else:     
+                i+=1
+                j+=1
+            
+            count+=1
+  
+        else:   
+            i+=1
+            j+=1
+  
+    if i < m or j < n: 
+        count+=1
+  
+    return count == 1
