7eba7377f2626343406ad9e91da00c3c2e8dd666
UpInStratosphere <38412253+UpInStratosphere@users.noreply.github.com>
2019-05-03 11:06:44 -0400

    Update reverse-vowels-of-a-string.py

--- 345.reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345.reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -7,8 +7,8 @@ class Solution(object):
     :type s: str
     :rtype: str
     """
-    vowels = set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"])
-    s = list(s)
+    vowels = set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]) #turn a list of strings into a set to have O(1) search time
+    s = list(s) #turn the given string into a list. otherwise, everytime swapping would make a new string. this is an optimization
     start, end = 0, len(s) - 1
     while start < end:
       if s[start] not in vowels:
@@ -19,4 +19,4 @@ class Solution(object):
         s[start], s[end] = s[end], s[start]
         start += 1
         end -= 1
-    return "".join(s)
+    return "".join(s) #turn the modified list into the answer string
